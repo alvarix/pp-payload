4fb8bb33a15ae93cae4d49039cc4dfa4d93107f6
alvar <alvarix@gmail.com>
2026-04-24 18:21:57 -0400

    login cookie > 30days

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -5,7 +5,9 @@ export const Users: CollectionConfig = {
   admin: {
     useAsTitle: 'email',
   },
-  auth: true,
+  auth: {
+    tokenExpiration: 60 * 60 * 24 * 30, // 30 days
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
